d05109c08b43f6e418e46c60eede732200938ad2
Marsnaut <jhuang5132@gmail.com>
2018-09-13 00:45:28 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,38 +1,118 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
 
+const deck = document.querySelector('.deck')
+const cards = [...document.querySelectorAll('.card')];
+const moveCount = document.querySelector('.moves');
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+let cardList = ['fa-diamond', 'fa-diamond',
+								'fa-paper-plane-o', 'fa-paper-plane-o',
+								'fa-anchor', 'fa-anchor',
+								'fa-bolt', 'fa-bolt',
+								'fa-cube', 'fa-cube',
+								'fa-leaf', 'fa-leaf',
+								'fa-bicycle', 'fa-bicycle',
+								'fa-bomb', 'fa-bomb'
+							];
+
+const generateCard = (card) => {;
+	return `<li class="card"><i class="fa ${card}"></i>`;
+}
+
+const initGame = () => {
+	let cardHTML = shuffle(cardList).map((card) => {
+		return generateCard(card);
+	}).join('');
+	deck.innerHTML = cardHTML;
+}
+
+initGame();
+
+
+
+let activeCards = [];
+let count = 0;
+
+const toggleCard = (card) => {
+	card.classList.toggle('open');
+	card.classList.toggle('show');
+}
+
+const addActiveCard = (card) => {
+	activeCards.push(card);
+}
+
+const checkMatch = () => {
+	if (activeCards[0].firstElementChild.className === activeCards[1].firstElementChild.className) {
+		activeCards[0].classList.toggle('match');
+		activeCards[1].classList.toggle('match');
+		activeCards = [];
+	} else {
+		const unFlip = setTimeout(() => {
+			toggleCard(activeCards[0]);
+			toggleCard(activeCards[1]);
+			activeCards = [];
+		}, 1000);
+		
+		// if (activeCards.length >= 3) {
+		// 	clearTimeout(unFlip);
+		// };
+
+	}
+	console.log(count);
+}
+
+const updateMoves = () => {
+	count++;
+
+	moveCount.innerHTML = count;
+}
+
+deck.addEventListener('click', (e) => {
+	const card = e.target;
+	
+	if (activeCards.length < 2) {
+		toggleCard(card);
+		addActiveCard(card);
+	}
+
+	if (activeCards.length === 2) {
+		checkMatch();
+		updateMoves();
+	}
+	
+})
+
+
+
+// const restart = document.querySelectorAll('#restart');
+// restart.addEventListener('click', () => {
+	
+// 	initGame();
+// })
+
+
+// const isGameWon = () => {
+	// 	const win = cards.every((card) => {
+	// 		card.classList.contains('match');
+	// 	})
+	
+	// 	if (win) {
+	//		alert('Your score is')
+	// 	}
+	
+	// }
+	
